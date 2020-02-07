147bd3485808a5f70793db8b718a05a1aa2b9518
Enzo <enzod98@hotmail.com>
2020-02-07 19:22:15 -0300

    Agregados estilos y contenido al index

--- index.php
+++ index.php
@@ -23,11 +23,68 @@
                 <label for="telefono">Telefono:</label>
                 <input type="tel" placeholder="Número de teléfono" id="telefono">
             </div>
-            <div class="campo enviar">
-                <input type="submit" value="Añadir">
-            </div>
+        </div>
+        <div class="campo enviar">
+            <input type="submit" value="Añadir">
         </div>
     </form>
+</div>
+
+<div class="bg-blanco contenedor sombra contactos">
+    <div class="contenedor-contactos">
+        <h2>Contactos</h2>
+
+        <input type="text" id="buscar" class="buscador sombra" placeholder="Buscar Contactos">
+
+        <p class="total-contactos"><span>2</span> Contactos</p>
+        <div class="contenedor-tabla">
+            <table id="listado-contactos" class="listado-contactos">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Empresa</th>
+                        <th>Teléfono</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Enzo</td>
+                        <td>Rieder & Cia</td>
+                        <td>123456</td>
+                        <td>
+                            <a href="#" class="btn-editar btn"><i class="fas fa-pen-square"></i></a>
+                            <button type="button" class="btn-borrar btn" data-id="1">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Enzo</td>
+                        <td>Rieder & Cia</td>
+                        <td>123456</td>
+                        <td>
+                            <a href="#" class="btn-editar btn"><i class="fas fa-pen-square"></i></a>
+                            <button type="button" class="btn-borrar btn" data-id="1">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Enzo</td>
+                        <td>Rieder & Cia</td>
+                        <td>123456</td>
+                        <td>
+                            <a href="#" class="btn-editar btn"><i class="fas fa-pen-square"></i></a>
+                            <button type="button" class="btn-borrar btn" data-id="1">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 
