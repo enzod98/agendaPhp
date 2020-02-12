84afe4edde0feeb0d3af750b4f6a98a5b4bd9b10
enzo <enzod98@hotmail.com>
2020-02-12 10:18:43 -0300

    Añadida validación JavaScript y todo lo dado hasta la clase 369

--- inc/layout/formulario.php
+++ inc/layout/formulario.php
@@ -13,6 +13,7 @@
     </div>
 </div>
 <div class="campo enviar">
+    <input type="hidden" id="accion" value="crear">
     <input type="submit" value="Añadir">
 </div>
 </div>
--- js/app.js
+++ js/app.js
@@ -13,11 +13,73 @@ function leerFormulario(e) {
     const nombre = document.querySelector('#nombre').value;
     const empresa = document.querySelector('#empresa').value;
     const telefono = document.querySelector('#telefono').value;
+    accion = document.querySelector('#accion').value;
 
     if (nombre === '' || empresa === '' || telefono === '') {
-        console.log('Al menos un campo está vacío')
+        //Dos parámetros: texto y clase
+        mostrarNotificacion('Todos los campos son obligatorios', 'error');
     } else {
-        console.log('Usted ingresó el nombre ' + nombre);
+        //Pasa la validación y hace un llamado a Ajax
+        const infoContacto = new FormData();
+        infoContacto.append('nombre', nombre);
+        infoContacto.append('empresa', empresa);
+        infoContacto.append('telefono', telefono);
+        infoContacto.append('accion', accion);
+
+        if (accion === 'crear') {
+            //Crearemos un nuevo contacto
+            insertarBD(infoContacto);
+        } else {
+            //editar el contacto
+        }
     }
 
+}
+
+/* Inserta en la base de datos vía Ajax */
+function insertarBD(datos) {
+    //  Llamado a Ajax
+
+    //crear el objeto
+    const xhr = new XMLHttpRequest();
+
+    //abrir la conexión
+    xhr.open('POST', 'inc/modelos/modelos-contactos.php', true);
+
+    //pasar los datos
+    xhr.onload = function() {
+        if (this.status === 200) {
+            console.log(JSON.parse(xhr.responseText));
+            //leemos la respuesta de php
+            const respuesta = JSON.parse(xhr.responseText);
+            console.log(respuesta.empresa);
+        }
+    }
+
+    //enviar los datos
+    xhr.send(datos);
+}
+
+//Notificación en pantalla
+function mostrarNotificacion(mensaje, clase) {
+    const notificacion = document.createElement('div');
+    notificacion.classList.add(clase, 'notificacion', 'sombra');
+    notificacion.textContent = mensaje;
+
+    //Formulario
+    formularioContactos.insertBefore(notificacion, document.querySelector('form legend'));
+
+    //Ocultar y mostrar la notificación
+
+    setTimeout(() => {
+        notificacion.classList.add('visible');
+
+        setTimeout(() => {
+            notificacion.classList.remove('visible');
+            setTimeout(() => {
+                notificacion.remove();
+            }, 500);
+
+        }, 3000);
+    }, 100);
 }
