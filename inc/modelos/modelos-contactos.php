e3dda8c55fedadb585d61e3a016f7f732c192f22
Enzo <enzod98@hotmail.com>
2020-02-13 09:48:09 -0300

    Agregada la función de añadir registros a la BD

--- inc/modelos/modelos-contactos.php
+++ inc/modelos/modelos-contactos.php
@@ -1,3 +1,37 @@
 <?php
-    echo json_encode($_POST);
+    if ($_POST['accion'] == 'crear'){
+        //se crea un registro en la bd
+        require_once('../funciones/bd.php');
+
+        //Validar las entradas
+        $nombre= filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
+        $empresa= filter_var($_POST['empresa'], FILTER_SANITIZE_STRING);
+        $telefono= filter_var($_POST['telefono'], FILTER_SANITIZE_STRING);
+
+        try{
+            $stmt = $conn->prepare("INSERT INTO contactos (nombre, empresa, telefono) VALUES (?, ?, ?)");
+            $stmt->bind_param('sss', $nombre, $empresa, $telefono);
+            $stmt->execute();
+            if($stmt->affected_rows == 1){
+                $respuesta= array(
+                    'respuesta' => 'correcto',
+                    'id_insertado' => $stmt->insert_id,
+                    'datos' => array(
+                        'id_insertado' => $stmt->insert_id,
+                        'nombre'=> $nombre,
+                        'empresa'=> $empresa,
+                        'telefono'=> $telefono
+                    )
+                );
+            }
+            $stmt->close();
+            $conn->close();
+        } catch(Exception $e){
+            $respuesta = array(
+                'error' => $e->getMessage()
+            );
+        }
+
+        echo json_encode($respuesta);
+    }
 ?>
